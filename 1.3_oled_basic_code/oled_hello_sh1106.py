9ae6d410e508a2be18674cc0931a513820fab1e6
AlAminAshik <alaminashik890.bd@gmail.com>
2025-05-24 23:41:20 +0600

    comments and directions added for code use

--- 1.3_oled_basic_code/oled_hello_sh1106.py
+++ 1.3_oled_basic_code/oled_hello_sh1106.py
@@ -1,20 +1,32 @@
-from luma.core.interface.serial import i2c
+# This code utilizes the luma library to display on oled display
+# steps to run this code
+# we need to get inside virtual environment
+# if already created, go inside the env folder and type "source bin/activate"
+# once inside, go back (cd ..) and type "python3 oled_hello_sh1106.py"
+# if error persists check for i2c connected device by typing "sudo i2cdetect -y 1"
+# Note: installing any libary using "pip" requires you to stay inside the virtual environment
+# Note: This is interchangable with ssd1306 oled display
+
+from luma.core.interface.serial import i2c		#pip install luma.oled
 from luma.oled.device import sh1106
-from PIL import Image, ImageDraw, ImageFont
-import time
+from PIL import Image, ImageDraw, ImageFont		#pip install pillow
+import time										#for adding delay
 
-serial = i2c(port=1, address=0x3c)
-device = sh1106(serial)
+# initialize i2c display
+device = sh1106(i2c(port=1, address=0x3c)) 		#displaytype(primary i2c port and address) 
 
+# create blank image
 image = Image.new("1", device.size)
 draw = ImageDraw.Draw(image)
 
+# load default font
 font = ImageFont.load_default()
 
-draw.rectangle(device.bounding_box, outline=0, fill=0)
+#Draw text
+draw.text((20,30), "hello world", font=font, fill=255)
 
-draw.text((20,30), "hello", font=font, fill=255)
-
+#Display image
 device.display(image)
 
+#show for 5 seconds
 time.sleep(5)
