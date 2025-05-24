8e1bbd25ea1f6d22ac2fcaa2e0afe5d1408e04ce
AlAminAshik <alaminashik890.bd@gmail.com>
2025-05-25 00:12:53 +0600

    different texts and shapes draw works!

--- 1.3_oled_basic_code/oled_images_shapes_sh1106.py
+++ 1.3_oled_basic_code/oled_images_shapes_sh1106.py
@@ -2,7 +2,7 @@
 # steps to run this code
 # we need to get inside virtual environment
 # if already created, go inside the env folder and type "source bin/activate"
-# once inside, go back (cd ..) and type "python3 oled_hello_sh1106.py"
+# once inside, go back (cd ..) and type "python3 oled_images_shapes_sh1106.py"
 # if error persists check for i2c connected device by typing "sudo i2cdetect -y 1"
 # Note: installing any libary using "pip" requires you to stay inside the virtual environment
 # Note: This is interchangable with ssd1306 oled display
@@ -19,11 +19,18 @@ device = sh1106(i2c(port=1, address=0x3c)) 		#displaytype(primary i2c port and a
 image = Image.new("1", device.size)
 draw = ImageDraw.Draw(image)
 
-# load default font
-font = ImageFont.load_default()
+# load fonts
+fontSmall = ImageFont.load_default()
+fontLarge = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf",20)
 
-#Draw text
-draw.text((20,30), "hello world", font=font, fill=255)
+# Draw shapes
+draw.rectangle((0,0,127,63), outline=255, fill=0)
+draw.ellipse((5,5,25,25), outline=255, fill=0)
+draw.line((0,32,128,32), fill=255)
+
+#Draw texts (texts should be after shapes)
+draw.text((30,8), "Small text", font=fontSmall, fill=255)
+draw.text((10,38), "Big text", font=fontLarge, fill=255)
 
 #Display image
 device.display(image)
